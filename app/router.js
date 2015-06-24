ac22a322b83c95c931bab5eb9221c6aa82106613
Brent Bailey & Garrett Olson <student@epicodus.com>
2015-06-24 15:41:19 -0700

    Get simulated authentication working via login page, link to match page from user profile

--- app/router.js
+++ app/router.js
@@ -5,14 +5,29 @@ var Router = Ember.Router.extend({
   location: config.locationType
 });
 
+// Router.map(function() {
+//   this.route('index', {path: '/'});
+//   this.resource('users/new', {path: 'new-user'});
+//   this.route('user');
+//   this.resource('users');
+//   this.resource('user', {path: 'users/:user_id'}, function() {
+//     this.route('match');
+//   });
+//   //this.resource('match', {path: 'users/:user_id/match'});
+//   this.resource('match');
+//   this.route('conversation');
+// });
+
 Router.map(function() {
   this.route('index', {path: '/'});
-  this.resource('users/new', {path: 'new-user'});
-  this.route('user');
   this.resource('users');
-  this.resource('user', {path: 'users/:user_id'});
-  //this.resource('match', { path: 'match/:match_id'});
-  this.resource('match');
+  this.resource('users/new', {path: '/new-user'});
+
+  this.resource('user', {path: '/users/:user_id'}, function() {
+    //this.route('match');
+  });
+
+  this.resource('match', {path: '/users/:user_id/match'});
   this.route('conversation');
 });
 
