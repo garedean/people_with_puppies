3992b5e50b7576878c2cfc783017ef5e016c0a35
andrew burt <andrew@ampletorque.com>
2015-06-26 11:31:48 -0700

    pre-merge

--- app/controllers/conversation.js
+++ app/controllers/conversation.js
@@ -9,48 +9,32 @@ export default Ember.ArrayController.extend({
         var newMessage = this.store.createRecord('message', {
           body: this.get('body')
         });
-        newMessage.save();
-
         var newConversation = this.store.createRecord('conversation');
-        newConversation.get('messages').pushObject(newMessage);
-        // newConversation.messages.pushObject(newMessage);
-        newConversation.save();
 
-        //
-        // var conversationData = {
-        //   // name: this.get('name'),
-        //   body: this.get('body')
-        // };
+        newMessage.save().then(function() {
+          newConversation.get('messages').pushObject(newMessage);
+          newConversation.save();
 
-
-
-        // newConversationModel.messages.pushObject(conversationData);
-        // newConversationModel.save();
-
-
+        });
 
         this.setProperties({
           messageTo: '',
           body: ''
         });
 
-
         var firstUser = this.get('controllers.user.model');
         firstUser.get('conversations').pushObject(newConversation);
-        // this.get('model').reload();
         firstUser.save();
 
   //
 
         // var secondUserName = this.get('messageTo');
-var secondUserId = this.model.get('recipient_id'); //this doesn't work obviously
+        var secondUserId = this.model.get('recipient_id'); //this doesn't work obviously
 debugger;
         var secondUser = this.store.find('user', { id: secondUserId });
         // var secondUser2 = this.store.find('user', "-Jsbvs12QPglBJ_wNjhk");
 
-  // debugger;
         secondUser.get('conversations').pushObject(newConversation);
-        // this.get('model').reload();
         secondUser.save();
 
         this.transitionTo('user', firstUser.id);
--- app/routes/conversation.js
+++ app/routes/conversation.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    return this.store.find('conversation', { users: params.user_id});
+    // debugger;
+    // var myConversation = "";
+    var myConversation = this.store.find('conversation', { users: params.user_id});
+// this.store.find('conversation', { users: params.user_id}).then(function(convIn){myConversation = convIn;})
+    // myConversation.then(function(){return myConversation;});
+    return myConversation;
   }
 });
